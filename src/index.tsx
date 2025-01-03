0ca38c7623443015f4e411b86117e54a7fd5f162
Hanna Kuchynski <hannakuchynski@Hannas-MBP.fios-router.home>
2025-01-03 10:45:45 -0500

    added markdown editor

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import ReactDOM from 'react-dom/client';
-import CodeCell from './components/code-cell';
+import TextEditor from './components/text-editor';
 
 const el = document.getElementById('root');
 
@@ -9,8 +9,7 @@ const root = ReactDOM.createRoot(el!);
 const App = () => {
   return (
     <div>
-      <CodeCell />
-      {/* <CodeCell /> */}
+      <TextEditor />
     </div>
   );
 };
